38f995a0ad76ac9eb9c4faa2756d225e7f3e8874
SNKotV <48939733+SNKotV@users.noreply.github.com>
2020-02-14 13:46:21 +0300

    fixed

--- caesar chiper/Encoder.java
+++ caesar chiper/Encoder.java
@@ -10,7 +10,7 @@ public class Encoder {
     private static int shift;
     private static File input;
     private static File output;
-
+    
     private static class TextInfo {
         public static int smallRus = 0;
         public static int capitalRus = 0;
@@ -66,17 +66,21 @@ public class Encoder {
                 case '\u0417': case '\u0418': case '\u0419': case '\u041a': case '\u041b': case '\u041c': case '\u041d': 
                 case '\u041e': case '\u041f': case '\u0420': case '\u0421': case '\u0422': case '\u0423': case '\u0424': 
                 case '\u0425': case '\u0426': case '\u0427': case '\u0428': case '\u0429': case '\u042a': case '\u042b': 
-                case '\u042c': case '\u042d': case '\u042e': case '\u042f':
+                case '\u042c': case '\u042d': case '\u042e': case '\u042f': case '\u0401':
                                                                 TextInfo.capitalRus++;
-                                                                chars[i] = (char)(((chars[i] - '\u0410' + shift) % 32) + '\u0410');
+                                                                int capsNumer = Encoder.getNumberByCode(chars[i], true);
+                                                                capsNumer = (capsNumer + shift % 33 + 33) % 33;
+                                                                chars[i] = Encoder.getLetterByNumber(capsNumer, true);
                                                                 break; 
                 case '\u0430': case '\u0431': case '\u0432': case '\u0433': case '\u0434': case '\u0435': case '\u0436': 
                 case '\u0437': case '\u0438': case '\u0439': case '\u043a': case '\u043b': case '\u043c': case '\u043d': 
                 case '\u043e': case '\u043f': case '\u0440': case '\u0441': case '\u0442': case '\u0443': case '\u0444': 
                 case '\u0445': case '\u0446': case '\u0447': case '\u0448': case '\u0449': case '\u044a': case '\u044b': 
-                case '\u044c': case '\u044d': case '\u044e': case '\u044f':   
+                case '\u044c': case '\u044d': case '\u044e': case '\u044f': case '\u0451':
                                                                 TextInfo.smallRus++;
-                                                                chars[i] = (char)(((chars[i] - '\u0430' + shift) % 32) + '\u0430');
+                                                                int smallNumer = Encoder.getNumberByCode(chars[i], false);
+                                                                smallNumer = (smallNumer + shift % 33 + 33) % 33;
+                                                                chars[i] = Encoder.getLetterByNumber(smallNumer, false);
                                                                 break;
                 case '0': case '1': case '2': case '3': case '4': case '5': case '6': 
                 case '7': case '8': case '9':                   
@@ -87,6 +91,39 @@ public class Encoder {
                               
             }   
         return new String(chars);
+    }
+
+    private static int getNumberByCode(char letter, boolean capital) {
+        if (capital)    {
+            if (letter == '\u0401')
+                return 6;
+            else if (letter <= '\u0415')
+                return letter - '\u0410';
+            else 
+                return letter - '\u0410' + 1;  
+        } else {
+            if (letter == '\u0451')
+                return 6;
+            else if (letter <= '\u0435')
+                return letter - '\u0430';
+            else 
+                return letter - '\u0430' + 1;            
+        }
+    }
+
+    private static char getLetterByNumber(int number, boolean capital) {
+        if (number <= 5 && capital)
+            return (char)(number + '\u0410');
+        else if (number <= 5)
+            return (char)(number + '\u0430');
+        else if (number == 6 && capital)
+            return '\u0401';
+        else if (number == 6)
+            return '\u0451';
+        else if (number > 5 && capital)
+            return (char)(number + '\u0410' - 1);
+        else 
+            return (char)(number + '\u0430' - 1);
     }
 
     public static void main(String[] args) {
